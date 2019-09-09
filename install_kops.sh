e272a446dcfba18f7bc89e72631281a62919812f
Ubuntu <ubuntu@ip-172-31-24-138.ec2.internal>
2019-09-09 03:54:55 +0000

    test

--- install_kops.sh
+++ install_kops.sh
@@ -3,7 +3,7 @@ export KOPS_STATE_STORE=s3://$2
 sed -i "s/NAME/$1/g" kops.yaml
 sed -i "s/NAME/$1/g" kopsig.yaml
 sed -i "s/BUCKET/$2/g" kops.yaml
-kops create -f kops.yaml
-kops create -f kopsig.yaml
-kops create secret sshpublickey admin -i ~/.ssh/id_rsa.pub --name $1
+kops create -f kops.yaml --name $1 --state s3://$2
+kops create -f kopsig.yaml  --name $1 --state s3://$2
+kops create secret sshpublickey admin -i ~/.ssh/id_rsa.pub --name $1 --state s3://$2
 #kops update cluster $1 --yes
